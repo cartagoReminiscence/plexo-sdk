d20102c2c7b0a9bfaeb0454ead65829fc96635c8
Bregy Malpartida <bregy@minsky.cc>
2024-03-07 13:15:45 -0500

    feat: refactor main.rs file with minimal example of use

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,7 @@ use std::{error::Error, sync::Arc};
 
 use dotenv::dotenv;
 
-use plexo_sdk::{
-    backend::engine::{SDKConfig, SDKEngine},
-    cognition::{
-        operations::{SubdivideTaskInputBuilder, TaskSuggestionInputBuilder},
-        v2::{operations::CognitionOperationsV2, projects::ProjectSuggestionInputBuilder},
-    },
-    common::commons::SortOrder,
-    resources::{
-        projects::operations::{GetProjectsInputBuilder, ProjectCrudOperations},
-        tasks::operations::{GetTasksInputBuilder, TaskCrudOperations},
-    },
-};
+use plexo_sdk::backend::engine::{SDKConfig, SDKEngine};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -26,55 +15,55 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     println!("version: {:?}", engine.version()?);
 
-    let projects = engine.get_projects(GetProjectsInputBuilder::default().build()?).await?;
-    let project = projects.first().unwrap().to_owned();
+    // let projects = engine.get_projects(GetProjectsInputBuilder::default().build()?).await?;
+    // let project = projects.first().unwrap().to_owned();
 
-    let task = engine
-        .get_tasks(
-            GetTasksInputBuilder::default()
-                .sort_by("created_at".to_string())
-                .sort_order(SortOrder::Asc)
-                .limit(1)
-                .build()
-                .ok(),
-        )
-        .await?
-        .first()
-        .unwrap()
-        .to_owned();
+    // let task = engine
+    //     .get_tasks(
+    //         GetTasksInputBuilder::default()
+    //             .sort_by("created_at".to_string())
+    //             .sort_order(SortOrder::Asc)
+    //             .limit(1)
+    //             .build()
+    //             .ok(),
+    //     )
+    //     .await?
+    //     .first()
+    //     .unwrap()
+    //     .to_owned();
 
-    let suggested_task = engine
-        .get_suggestions_v2(
-            TaskSuggestionInputBuilder::default()
-                .project_id(project.id)
-                .title("next big update".to_string())
-                .build()?,
-        )
-        .await?;
+    // let suggested_task = engine
+    //     .get_suggestions_v2(
+    //         TaskSuggestionInputBuilder::default()
+    //             .project_id(project.id)
+    //             .title("next big update".to_string())
+    //             .build()?,
+    //     )
+    //     .await?;
 
-    println!("suggestion: {:?}", suggested_task);
+    // println!("suggestion: {:?}", suggested_task);
 
-    let subdivided_tasks = engine
-        .subdivide_task_v2(
-            SubdivideTaskInputBuilder::default()
-                .task_id(task.id)
-                .subtasks(3)
-                .build()?,
-        )
-        .await?;
+    // let subdivided_tasks = engine
+    //     .subdivide_task_v2(
+    //         SubdivideTaskInputBuilder::default()
+    //             .task_id(task.id)
+    //             .subtasks(3)
+    //             .build()?,
+    //     )
+    //     .await?;
 
-    println!("subdivided tasks: {:?}", subdivided_tasks);
+    // println!("subdivided tasks: {:?}", subdivided_tasks);
 
-    let project_suggestion = engine
-        .get_project_suggestion(
-            ProjectSuggestionInputBuilder::default()
-                .description("A new project based on modern web".to_string())
-                .generate_tasks_number(3)
-                .build()?,
-        )
-        .await?;
+    // let project_suggestion = engine
+    //     .get_project_suggestion(
+    //         ProjectSuggestionInputBuilder::default()
+    //             .description("A new project based on modern web".to_string())
+    //             .generate_tasks_number(3)
+    //             .build()?,
+    //     )
+    //     .await?;
 
-    println!("project suggestion: {:?}", project_suggestion);
+    // println!("project suggestion: {:?}", project_suggestion);
 
     Ok(())
 }
